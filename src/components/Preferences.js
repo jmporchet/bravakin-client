82f368aa8903dbaf4de5244dd1954833daafa5aa
= <jm@jmporchet.ch>
2017-08-16 09:46:37 +0200

    wip: preferences page

--- src/components/Preferences.js
+++ src/components/Preferences.js
@@ -1,5 +1,41 @@
 import React from 'react';
 
-const Preferences = () => <div><h1>Preferences</h1></div>;
+import { ListGroup, ListGroupItem, Badge } from 'reactstrap';
 
-export default Preferences;
+export default class Preferences extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col">
+          </div>
+          <div className="col">
+            Profile information
+          </div>
+          <div className="col">
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            Interesting users
+            <ListGroup>
+              <ListGroupItem className="justify-content-between">Cras justo odio <i className="fa fa-times" aria-hidden="true"></i></ListGroupItem>
+              <ListGroupItem className="justify-content-between">Dapibus ac facilisis in <i className="fa fa-times" aria-hidden="true"></i></ListGroupItem>
+              <ListGroupItem className="justify-content-between">Morbi leo risus <i className="fa fa-times" aria-hidden="true"></i></ListGroupItem>
+            </ListGroup>
+          </div>
+          <div className="col">
+          </div>
+          <div className="col">
+            Interesting hashtags
+            <ListGroup>
+              <ListGroupItem className="justify-content-between">Cras justo odio <i className="fa fa-times" aria-hidden="true"></i></ListGroupItem>
+              <ListGroupItem className="justify-content-between">Dapibus ac facilisis in <i className="fa fa-times" aria-hidden="true"></i></ListGroupItem>
+              <ListGroupItem className="justify-content-between">Morbi leo risus <i className="fa fa-times" aria-hidden="true"></i></ListGroupItem>
+            </ListGroup>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
