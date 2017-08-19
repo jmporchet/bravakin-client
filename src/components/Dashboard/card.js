6394021470572578102d61ff835ba6a5a4d03a79
alexsicart <alex@sharge.io>
2017-08-19 12:51:06 +0200

    feat: direction working

--- src/components/Dashboard/card.js
+++ src/components/Dashboard/card.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Swing from 'react-swing';
+import { Direction } from 'swing';
+
 import styles from './card.css';
 import Photo from './photo.jpg'
-import Swing, { Stack, Card, Direction } from './swing.js';
 import faker from 'faker';
 import FakeData from './fakeData.js';
 
@@ -30,53 +32,55 @@ class Card2 extends Component {
     return FakeData.map((data) =>
     <div className="card" ref={data.id}
       style={{backgroundImage: "url(" + data.image + ")", backgroundSize: 'cover', backgroundPosition: 'center'}}>{data.username}</div>  )
-}
-
-
-// throwOut Method
-throwCard() {
-  // Swing Card Directions
-  console.log('Swing.DIRECTION', Swing.DIRECTION);
-
-  // Swing Component Childrens refs
-  const target = this.refs.stack.refs.card2;
-
-  // get Target Dom Element
-  const el = ReactDOM.findDOMNode(target);
-
-  // stack.getCard
-  const card = this.state.stack.getCard(el);
-
-  // throwOut method call
-  card.throwOut(100, 200, Swing.DIRECTION.RIGHT);
-}
-
-render() {
-  return (
-    <div>
-      <div id="viewport">
-
-        {/*
-          Swing Element
-          */}
-          <Swing
-            className="stack"
-            tagName="div"
-            setStack={(stack)=> this.setState({stack:stack})}
-            ref="stack"
-            throwout={(e)=>console.log('throwout',e)}
-            >
-            {/*
-              children elements is will be Card
-              */}
-              {this.fakeData()}
-            </Swing>
-          </div>
-          <div className="control">
-          </div>
-        </div>
-      )
     }
-  }
 
-  export default Card2;
+
+    // throwOut Method
+    throwCard() {
+      // Swing Card Directions
+      console.log('Swing.DIRECTION', Swing.DIRECTION);
+      // Directions using fetch
+
+      // Swing Component Childrens refs
+      const target = this.refs.stack.refs.card2;
+
+      // get Target Dom Element
+      const el = ReactDOM.findDOMNode(target);
+
+      // stack.getCard
+      const card = this.state.stack.getCard(el);
+
+      // throwOut method call
+      card.throwOut(100, 200, Swing.DIRECTION.RIGHT);
+    }
+
+    render() {
+      return (
+        <div>
+          <div id="viewport">
+
+            {/*
+              Swing Element
+              */}
+              <Swing
+                className="stack"
+                tagName="div"
+                setStack={(stack)=> this.setState({stack:stack})}
+                ref="stack"
+                throwout={(e)=> {
+                  if(e.throwDirection === Direction.LEFT) console.log('Noooooooo')
+                  else console.log('I like it');
+                }}
+                >
+                  {/* children elements is will be Card */}
+                  {this.fakeData()}
+                </Swing>
+              </div>
+              <div className="control">
+              </div>
+            </div>
+          )
+        }
+      }
+
+      export default Card2;
