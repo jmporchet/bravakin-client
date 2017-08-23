29e2dedd6d66e5e8767f4f770fe991ba1336528b
alexsicart <alex@sharge.io>
2017-08-23 13:41:58 +0200

    minor bugs

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
