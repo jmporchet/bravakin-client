2f3bf2debc0065a98ce9bfa6a47de2514c41855d
alexsicart <alex@sharge.io>
2017-08-23 12:19:03 +0200

    feat: design

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -1,18 +1,41 @@
 import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom'
 
-import { Link } from 'react-router-dom';
+class TopMenu extends React.Component {
+  constructor(props) {
+    super(props);
 
-const TopMenu = () => (
-  <div>
-    <ul>
-      <li><Link to="/">Home</Link></li>
-      <li><Link to="/dashboard">Dashboard</Link></li>
-      <li><Link to="/performance">Performance</Link></li>
-      <li><Link to="/preferences">Preferences</Link></li>
-    </ul>
-
-    <hr/>
-  </div>
-);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="faded" light toggleable>
+          <NavbarToggler right onClick={this.toggle} />
+          <NavbarBrand href="/">Bravakin</NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/performance">Performance</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/preferences">Preferences</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
 
 export default TopMenu;
