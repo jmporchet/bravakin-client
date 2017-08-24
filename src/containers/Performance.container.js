956f38c97ff6a08b32271b328fc1f95229fe8f2d
= <jm@jmporchet.ch>
2017-08-24 10:00:46 +0200

    fix: performance chart to work with real data

--- src/containers/Performance.container.js
+++ src/containers/Performance.container.js
@@ -11,7 +11,10 @@ class PerformanceContainer extends React.Component {
 
   constructor () {
     super();
-    this.state = { access_token: null };
+    this.state = {
+      access_token: null,
+      commentsAndLikes: null
+    };
   }
 
   componentWillMount () {
@@ -30,43 +33,27 @@ class PerformanceContainer extends React.Component {
       .then((response) => response.json())
       .then((response) => {
         // inject into graph
+        const processedData = response.stats.map(el => {
+          return Object.assign({}, el, {
+            date: new Date(el.date)
+          });
+        });
+        console.log(processedData);
+        this.setState({ commentsAndLikes: processedData })
       })
       .catch((error) => {
         console.error(error);
       });
-
-
-      this.commentsAndLikes = [
-        {date: new Date(Date.now() - (1 * 3600000)), likes: 12, followers: 1, comments: 1},
-        {date: new Date(Date.now() - (2 * 3600000)), likes: 2, followers: 2, comments: 5},
-        {date: new Date(Date.now() - (3 * 3600000)), likes: 0, followers: 2, comments: 0},
-        {date: new Date(Date.now() - (4 * 3600000)), likes: 12, followers: 2, comments: 5},
-        {date: new Date(Date.now() - (5 * 3600000)), likes: 7, followers: 2, comments: 5},
-        {date: new Date(Date.now() - (6 * 3600000)), likes: 0, followers: 2, comments: 2},
-        {date: new Date(Date.now() - (7 * 3600000)), likes: 40, followers: 2, comments: 5},
-        {date: new Date(Date.now() - (8 * 3600000)), likes: 1, followers: 2, comments: 1},
-        {date: new Date(Date.now() - (9 * 3600000)), likes: 12, followers: 2, comments: 1},
-        {date: new Date(Date.now() - (10 * 3600000)), likes: 1, followers: 2, comments: 1},
-        {date: new Date(Date.now() - (11 * 3600000)), likes: 5, followers: 2, comments: 2},
-        {date: new Date(Date.now() - (12 * 3600000)), likes: 1, followers: 2, comments: 0},
-        {date: new Date(Date.now() - (13 * 3600000)), likes: 13, followers: 2, comments: 0},
-        {date: new Date(Date.now() - (14 * 3600000)), likes: 1, followers: 22, comments: 0},
-        {date: new Date(Date.now() - (15 * 3600000)), likes: 22, followers: 2, comments: 0},
-        {date: new Date(Date.now() - (16 * 3600000)), likes: 41, followers: 2, comments: 3},
-        {date: new Date(Date.now() - (17 * 3600000)), likes: 38, followers: 2, comments: 7},
-        {date: new Date(Date.now() - (19 * 3600000)), likes: 33, followers: 2, comments: 1},
-        {date: new Date(Date.now() - (19 * 3600000)), likes: 16, followers: 2, comments: 2},
-        {date: new Date(Date.now() - (20 * 3600000)), likes: 20, followers: 2, comments: 2}
-      ];
   }
 
   render () {
+    if(!this.state.commentsAndLikes) return null;
     return (
       <div>
         <h2>Likes and Comments performance</h2>
-        <LineAndBarsChart data={this.commentsAndLikes} width="800" height="460" />
+        <LineAndBarsChart data={this.state.commentsAndLikes} width="800" height="460" />
         <h2>Followers performance</h2>
-        <Linechart data={this.commentsAndLikes} width="800" height="460" />
+        <Linechart data={this.state.commentsAndLikes} width="800" height="460" />
         {/* <h2>Best time to post</h2>
         <Heatmap width="800" height="460" /> */}
         <h2>Map of influence</h2>
